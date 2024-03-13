e84c305d479fcc10303585d979b93521ee2a351f
Víctor Martínez <vmmansilla93@gmail.com>
2024-03-13 16:37:58 +0100

    Style breadcrumb.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from '../../shared/services/layout.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private readonly layoutService: LayoutService
+  ) {
+    layoutService.breadcrumb.set([]);
   }
 
 }
